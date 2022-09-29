dbd1255293a1d6757bfc17ef47d9a24f26c79d53
jose2432 <josemp366@ciencias.unam.mx>
2022-09-29 16:36:10 -0500

    Tarea1

---
+++ Tareas/Tarea1/Solucion/tarea_1.py
@@ -0,0 +1,19 @@
+#!coding:utf8
+
+def ejercicio_2(lista_secuencias):
+    L = []
+    ...
+    return L
+
+def ejercicio_3(archivo):
+    L = []
+    ...
+    return L
+
+def ejercicio_4(M):
+    x = 0.
+    ...
+    return x 
+
+
+
